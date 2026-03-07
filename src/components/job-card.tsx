4f8fe35d2a83111b696ea22e9cf15664f87e8553
Firdig Alfalakhi <Alfalakhie@gmail.com>
2026-03-07 12:03:42 +0700

    fix: swap between place and position in job card

--- src/components/job-card.tsx
+++ src/components/job-card.tsx
@@ -8,16 +8,20 @@ interface JobCardProps {
   link: string;
 }
 
-export default function JobCard({ data }: { data: JobCardProps } ) {
+export default function JobCard({ data }: { data: JobCardProps }) {
   return (
-    <a href={data.link} target="_blank" className="flex w-full items-center justify-between rounded-md bg-[#202022] p-4 sm:p-5 hover:bg-[#202022]/80">
+    <a
+      href={data.link}
+      target="_blank"
+      className="flex w-full items-center justify-between rounded-md bg-[#202022] p-4 hover:bg-[#202022]/65 sm:p-5"
+    >
       <div className="flex items-center gap-x-7">
         <div className="relative hidden h-9 w-9 overflow-hidden rounded-full sm:flex">
           <Image alt="Image" src={data.image} fill className="object-cover" />
         </div>
         <div>
-          <p className="text-sm font-semibold sm:text-base">{data.position}</p>
-          <p className="text-xs text-zinc-400 sm:text-sm">{data.place}</p>
+          <p className="text-sm font-semibold sm:text-base">{data.place}</p>
+          <p className="text-xs text-zinc-400 sm:text-sm">{data.position}</p>
         </div>
       </div>
       <div>
